fa141a77de95b1c8d4b9611db14d9eb3a4a80baf
pablituuu <keane.pa.si.in@gmai.com>
2023-01-25 11:17:13 -0500

    add function load template by id and add redirect page with env

--- src/designer/id.tsx
+++ src/designer/id.tsx
@@ -1,5 +1,5 @@
 import { Flex, useDisclosure } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { selectUser } from "../components/store/user/selector"
 import DesignEditor from "../components/DesignEditor"
@@ -9,6 +9,9 @@ import { generateId } from "../components/utils/unique"
 import * as api from "../.././src/components/services/api"
 import { useAppDispatch } from "../components/store/store"
 import { signInByToken } from "../components/store/user/action"
+import { useDesign, useEditor } from "@layerhub-pro/react"
+import useDesignEditorContext from "../components/hooks/useDesignEditorContext"
+const redirectHome = import.meta.env.VITE_REDIRECT_HOME
 
 const Designer: any = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -18,8 +21,13 @@ const Designer: any = () => {
   const navigate = useNavigate()
   const token = localStorage.getItem("token")
   const dispath = useAppDispatch()
+  const editor = useEditor()
+  const { setNamesPages } = useDesignEditorContext()
+  const design = useDesign()
 
-  console.log(user)
+  useEffect(() => {
+    design && lodaTemplateById()
+  }, [design])
 
   useEffect(() => {
     if (user) {
@@ -27,10 +35,26 @@ const Designer: any = () => {
     } else if (token) {
       token !== "" && dispath(signInByToken(token))
     } else {
-      window.location.href = "https://drawify.local/"
+      window.location.href = redirectHome
     }
     id === undefined && navigate(`/composer/${generateId("proj")}`)
   }, [user])
+
+  const lodaTemplateById = useCallback(async () => {
+    try {
+      if (design) {
+        const resolve: any = await api.getProjectById({ id })
+        setTimeout(() => {
+          design.setDesign(resolve)
+        }, 1000)
+        let sceneNames: string[] = []
+        for (const scn of resolve.scenes) {
+          sceneNames.push(scn.name)
+        }
+        setNamesPages(sceneNames)
+      }
+    } catch (err: any) {}
+  }, [id, editor])
 
   return (
     <Flex sx={{ height: "100vh", width: "100vw" }}>
