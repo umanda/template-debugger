655a3b25a2565146fbe3f6c78dbaeb124711401c
pablituuu <keane.pa.si.in@gmai.com>
2023-02-10 16:06:16 -0500

    add function order resources by drag and drop in panel layers

--- src/components/DesignEditor/components/Panels/panelItems/Layer.tsx
+++ src/components/DesignEditor/components/Panels/panelItems/Layer.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react"
-import { Box, Button, Center, Flex, Grid, GridItem, IconButton, Image, Spacer } from "@chakra-ui/react"
+import { Box, Button, Center, Flex, Grid, GridItem, IconButton, Image, List, ListItem, Spacer } from "@chakra-ui/react"
 import { useActiveObject, useActiveScene, useObjects } from "@layerhub-pro/react"
 import Lock from "../../../../Icons/Lock"
 import Unlock from "../../../../Icons/Unlock"
@@ -13,42 +13,127 @@ import CircleDown from "../../../../Icons/CircleDown"
 import Pencil from "../../../../Icons/Pencil"
 import LetterUpperCase from "../../../../Icons/LetterUpperCase"
 import useResourcesContext from "../../../../hooks/useResourcesContext"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
 export default function Layer() {
   const [objects, setObjects] = useState<any[]>([])
-  const activeScene: any = useActiveScene()
+  const activeScene = useActiveScene()
   const activeObject = useActiveObject()
   const { order } = useResourcesContext()
   const [indexDrag, setIndexDrag] = useState<number | null>(null)
+  const [changeOrder, setChangeOrder] = useState<{
+    cont: number | null
+    to: "front" | "back" | null
+    id: string | null
+  }>({
+    cont: null,
+    to: null,
+    id: null
+  })
+
+  useEffect(() => {
+    changeOrder.to !== null && handleChangeOrder()
+  }, [changeOrder.cont])
 
   useEffect(() => {
     setObjects(activeScene.layers.filter((e: any) => e.name !== "Initial Frame" && e.name !== "Custom").reverse())
   }, [activeScene, activeObject, order])
 
+  const handleChangeOrder = useCallback(() => {
+    if (changeOrder.to === "front") {
+      activeScene.layers.map((e, index) => {
+        if (index < changeOrder.cont) {
+          setTimeout(async () => activeScene.objects.bringForward(changeOrder.id), 100)
+        }
+      })
+    } else if (changeOrder.to === "back") {
+      activeScene.layers.map((e, index) => {
+        if (index < changeOrder.cont) {
+          setTimeout(() => activeScene.objects.sendBackwards(changeOrder.id), 100)
+        }
+      })
+    }
+    setChangeOrder({ ...changeOrder, to: null })
+  }, [changeOrder, activeScene])
+
+  const reorder = (list, startIndex, endIndex) => {
+    const resource = activeScene?.layers
+      ?.filter((e: any) => e.name !== "Initial Frame" && e.name !== "Custom")
+      ?.reverse()[startIndex]
+    if (startIndex - endIndex > 0) {
+      setChangeOrder({
+        cont: startIndex - endIndex,
+        to: "front",
+        id: resource?.id
+      })
+    } else if (startIndex - endIndex < 0) {
+      setChangeOrder({
+        cont: endIndex - startIndex,
+        to: "back",
+        id: resource?.id
+      })
+    }
+    const result = [...list]
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
   return (
     <Flex h="full" width="320px" borderRight="1px solid #DDDFE5" padding="0.5rem">
-      {objects.length === 0 && (
-        <Center w="full" h="full">
+      {objects.length === 0 ? (
+        <Center bg="red" w="full" h="full">
           No layer found
         </Center>
+      ) : (
+        <Flex w="280px" h="full">
+          <DragDropContext
+            onDragEnd={(result) => {
+              const { source, destination } = result
+              if (!destination) return
+              if (source.index === destination.index && source.droppableId === destination.droppableId) {
+                return
+              }
+              setObjects((prevTasks) => reorder(prevTasks, source.index, destination.index))
+            }}
+          >
+            <Flex w="full" h="full">
+              <Scrollable autoHide={true}>
+                <Droppable droppableId="key">
+                  {(droppableProvided) => (
+                    <List w="full" {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+                      {objects.map((object, index) => (
+                        <Draggable key={index} draggableId={String(index)} index={index}>
+                          {(draggableProvided) => (
+                            <ListItem
+                              {...draggableProvided.draggableProps}
+                              ref={draggableProvided.innerRef}
+                              {...draggableProvided.dragHandleProps}
+                            >
+                              <LayerItem
+                                indexDrag={indexDrag}
+                                setIndexDrag={setIndexDrag}
+                                setObjects={setObjects}
+                                locked={object.locked}
+                                key={index}
+                                id={object.id}
+                                name={object.name}
+                                visible={object.visible}
+                                index={index}
+                              />
+                            </ListItem>
+                          )}
+                        </Draggable>
+                      ))}
+                      {droppableProvided.placeholder}
+                    </List>
+                  )}
+                </Droppable>
+              </Scrollable>
+            </Flex>
+          </DragDropContext>
+        </Flex>
       )}
-      <Flex flexDirection="column" w="full" h="full">
-        <Scrollable autoHide={true}>
-          {objects.map((object, index) => (
-            <LayerItem
-              indexDrag={indexDrag}
-              setIndexDrag={setIndexDrag}
-              setObjects={setObjects}
-              locked={object.locked}
-              key={index}
-              id={object.id}
-              name={object.name}
-              visible={object.visible}
-              index={index}
-            />
-          ))}
-        </Scrollable>
-      </Flex>
     </Flex>
   )
 
@@ -69,10 +154,6 @@ export default function Layer() {
     const { order, setOrder } = useResourcesContext()
     const activeScene = useActiveScene()
     const object: any = activeScene.objects.findById(id)
-    const activeObject: any = useActiveObject()
-    const [indexTransfer, setIndexTransfer] = useState<number | null>(null)
-    const [transfer, setTransfer] = useState<boolean>(false)
-    const [drag, setDrag] = useState({ top: false, bot: false })
     const [stateIcon, setStateIcon] = React.useState({ lock: object[0]?.locked, eye: object[0]?.visible })
     const [state, setState] = React.useState<State>({
       name,
@@ -128,20 +209,9 @@ export default function Layer() {
       setOrder(!order)
     }, [activeScene, id, order])
 
-    const dataTransfer = useCallback(
-      (ev: React.DragEvent<HTMLDivElement>) => {
-        setIndexTransfer(index)
-        ev.dataTransfer.setData("resource", "image")
-      },
-      [index]
-    )
-
-    const dropTransfer = useCallback(() => {
-      setIndexTransfer(null)
-    }, [])
-
     return (
       <Flex
+        w="full"
         alignItems="center"
         height="45px"
         marginRight="10px"
@@ -153,58 +223,59 @@ export default function Layer() {
         borderBottom="1px"
         borderColor="linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.5578606442577031) 27%, rgba(0,212,255,0) 100%)"
       >
-        <Flex
-          zIndex={1}
-          w="full"
-          onDragStart={(e) => dataTransfer(e)}
-          onDragEnter={() => index !== indexTransfer && setTransfer(true)}
-          // onDragEnd={() => dropTransfer()}
-          // onDragEnd={(e) => console.log(e.dataTransfer.getData("layer"))}
-          draggable
-        >
-          <IconButton size="xs" aria-label="Search database" variant={"ghost"} icon={<Drag size={24} />} />
+        <Flex w="full">
+          <IconButton
+            onClick={() => activeScene.objects.select(id)}
+            _hover={{ cursor: "grab" }}
+            size="30px"
+            aria-label="Search database"
+            variant="ghost"
+            icon={<Drag size={24} />}
+          />
           {object[0]?.type === "StaticVector" ? (
             <Center
+              onClick={() => activeScene.objects.select(id)}
               borderRadius="md"
-              sx={{
-                alignItems: "center",
-                display: "flex",
-                ":hover": {
-                  backgroundColor: "brand.50",
-                  cursor: "pointer",
-                  color: "brand.500"
-                }
-              }}
+              alignItems="center"
+              display="flex"
+              _hover={{}}
             >
-              <Image draggable={false} w="30px" h="30px" maxH="40px" maxW="40px" src={object[0].src} alt="Resource" />
+              <Image w="30px" h="30px" maxH="40px" maxW="40px" src={object[0].src} alt="Resource" />
             </Center>
           ) : object[0]?.type === "StaticText" ? (
             <IconButton
+              onClick={() => activeScene.objects.select(id)}
+              _hover={{ cursor: "grab" }}
               size="30px"
               aria-label="Search database"
-              variant={"ghost"}
+              variant="ghost"
               icon={<LetterUpperCase size={24} />}
             />
           ) : (
-            <IconButton size="30px" aria-label="Search database" variant={"ghost"} icon={<Pencil size={24} />} />
-          )}
-          <Flex w="full" alignItems={"center"}>
-            <Button
-              size="xs"
-              variant="ghost"
-              w="full"
+            <IconButton
               onClick={() => activeScene.objects.select(id)}
-              justifyContent="left"
-              fontSize="14px"
-              fontWeight={activeObject?.id === id ? "extrabold" : "light"}
-            >
-              {object[0]?.name === "StaticText"
-                ? String(object[0]?.text).length <= 12
-                  ? object[0]?.text
-                  : `${object[0]?.text.substr(0, 12)}...`
-                : name}
-            </Button>
-          </Flex>
+              _hover={{ cursor: "grab" }}
+              size="30px"
+              aria-label="Search database"
+              variant="ghost"
+              icon={<Pencil size={24} />}
+            />
+          )}
+          <Center
+            onClick={() => activeScene.objects.select(id)}
+            borderRadius="md"
+            alignItems="center"
+            display="flex"
+            fontSize="12px"
+            _hover={{}}
+            marginLeft="10px"
+          >
+            {object[0]?.name === "StaticText"
+              ? String(object[0]?.text).length <= 12
+                ? object[0]?.text
+                : `${object[0]?.text.substr(0, 12)}...`
+              : name}
+          </Center>
           <Spacer />
           <IconButton onClick={handleFront} size="xs" aria-label="Up" variant={"ghost"} icon={<CircleUp size={15} />} />
           <IconButton
@@ -230,45 +301,6 @@ export default function Layer() {
             icon={<Trash size={24} />}
           />
         </Flex>
-        <Grid
-          gridTemplateRows="1fr 1fr"
-          zIndex={transfer ? 2 : 0}
-          onDragLeave={() => index !== indexTransfer && setTransfer(false)}
-          position="absolute"
-          w="full"
-          h="full"
-        >
-          <GridItem
-            borderRadius="md"
-            onDragEnter={(e) => {
-              setDrag({ ...drag, top: true })
-              e.dataTransfer.setData("layer", "layer")
-              // setIndexDrag(index)
-            }}
-            onDragLeave={(e) => {
-              // setIndexDrag(null)
-              setDrag({ ...drag, top: false })
-              e.dataTransfer.setData("layer", "")
-            }}
-            borderTop={drag.top ? "4px" : "0px"}
-            borderColor="brand.500"
-          />
-          <GridItem
-            borderRadius="md"
-            onDragEnter={(e) => {
-              setDrag({ ...drag, bot: true })
-              e.dataTransfer.setData("layer", "layer")
-              // setIndexDrag(index)
-            }}
-            onDragLeave={(e) => {
-              // setIndexDrag(null)
-              setDrag({ ...drag, bot: false })
-              e.dataTransfer.setData("layer", "")
-            }}
-            borderBottom={drag.bot ? "4px" : "0px"}
-            borderColor="brand.500"
-          />
-        </Grid>
       </Flex>
     )
   }
