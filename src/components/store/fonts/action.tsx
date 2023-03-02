165612ec4dcc80e5418f1582066a2ecb31e9a343
pablituuu <keane.pa.si.in@gmai.com>
2023-03-02 15:38:19 -0500

    fix error build

--- src/components/store/fonts/action.tsx
+++ src/components/store/fonts/action.tsx
@@ -7,23 +7,29 @@ export const setListUseFonts = createAction<IFont[]>("fonts/setUseFonts")
 export const setUseFont = createAction<IFont>("fonts/setUseFont")
 export const setCategoryFonts = createAction<ICategoryFonts>("fonts/setCategoryFonts")
 
-export const getFonts = createAsyncThunk<void, never, any>("fonts/getFonts", async (_, { dispatch }) => {
-  try {
-    const fonts = await api.getFonts()
-    dispatch(setFonts(fonts))
-  } catch (err) {
-    console.log("ERROR GETTING FONTS")
+export const getFonts = createAsyncThunk<void, never, any>(
+  "fonts/getFonts",
+  async (_, { dispatch, rejectWithValue }) => {
+    try {
+      const fonts = await api.getFonts()
+      dispatch(setFonts(fonts))
+    } catch (err) {
+      rejectWithValue(err)
+    }
   }
-})
-export const getListUseFonts = createAsyncThunk<void, never, any>("fonts/getUseFonts", async (_, { dispatch }) => {
-  try {
-    const fonts: any = await api.getListUseFonts()
-    dispatch(setListUseFonts(fonts))
-    return fonts
-  } catch (err) {
-    console.log("ERROR GETTING FONTS")
+)
+export const getListUseFonts = createAsyncThunk<void, never, any>(
+  "fonts/getUseFonts",
+  async (_, { dispatch, rejectWithValue }) => {
+    try {
+      const fonts: any = await api.getListUseFonts()
+      dispatch(setListUseFonts(fonts))
+      return fonts
+    } catch (err) {
+      rejectWithValue(err)
+    }
   }
-})
+)
 
 export const getUseFont = createAsyncThunk<void, IFont, any>(
   "fonts/getUseFont",
@@ -37,13 +43,15 @@ export const getUseFont = createAsyncThunk<void, IFont, any>(
   }
 )
 
-export const getCategoryFonts = createAsyncThunk<void, never, any>("fonts/getUseFont", async (font, { dispatch }) => {
-  try {
-    const resolve = await api.getCategoryFonts()
-    dispatch(setCategoryFonts(resolve))
-    return resolve
-  } catch (err) {
-    // null
-    console.log("ERROR GETTING FONTS")
+export const getCategoryFonts = createAsyncThunk<void, never, any>(
+  "fonts/getUseFont",
+  async (font, { dispatch, rejectWithValue }) => {
+    try {
+      const resolve = await api.getCategoryFonts()
+      dispatch(setCategoryFonts(resolve))
+      return resolve
+    } catch (err) {
+      rejectWithValue(err)
+    }
   }
-})
+)
