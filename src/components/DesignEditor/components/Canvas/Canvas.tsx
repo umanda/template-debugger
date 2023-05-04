27b9ab96094058d47af9080361c7fa5b28cf5b05
Pablito Jean Pool Silva Inca <keane.pa.si.in@gmail.com>
2023-05-04 10:17:59 -0500

    fixed guidelines

--- src/components/DesignEditor/components/Canvas/Canvas.tsx
+++ src/components/DesignEditor/components/Canvas/Canvas.tsx
@@ -164,7 +164,7 @@ export default function Canva() {
             },
             outsideVisible: true,
             guidelines: {
-              enabled: false,
+              enabled: true,
               color: ""
             },
             zoomToMode: "POINT",
