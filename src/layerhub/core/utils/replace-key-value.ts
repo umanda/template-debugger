1c9a6930ef1a032d4780100bc3654f138d49b1c1
Pablito Jean Pool Silva Inca <keane.pa.si.in@gmail.com>
2023-08-18 11:18:02 -0500

    fixed load escene in canva with magic generate

--- src/layerhub/core/utils/replace-key-value.ts
+++ src/layerhub/core/utils/replace-key-value.ts
@@ -6,37 +6,40 @@ export function replaceParamWithValue(layer: IStaticText, props: Record<string, 
   // @ts-ignore
   let textParams = layer.params as any[]
   function replaces() {
-    const matches = text.matchAll(REGEX_VAR)
+    const matches = text?.matchAll(REGEX_VAR)
     let i = 0
     let pieces = ""
-    for (const match of matches) {
-      const startIndex = match["index"]
-      const matchWord = match["0"]
+    if (matches !== undefined) {
+      for (const match of matches) {
+        const startIndex = match["index"]
+        const matchWord = match["0"]
 
-      if (i === 0) {
-        const initialSection = text.slice(0, startIndex)
-        pieces = pieces + initialSection
-      }
-
-      let key = matchWord.substring(0, matchWord.length)
-
-      const customParam = textParams.find((tp) => tp.key === key)
-
-      let updatedValue = matchWord.substring(1, matchWord.length - 1)
-
-      if (customParam) {
-        const customProp = props[customParam.name]
-        if (customProp) {
-          updatedValue = customProp
+        if (i === 0) {
+          const initialSection = text.slice(0, startIndex)
+          pieces = pieces + initialSection
         }
-      }
 
-      pieces = pieces + updatedValue
-      const lastPiece = text.slice(startIndex! + matchWord.length, text.length)
-      text = pieces + lastPiece
-      replaces()
-      return false
+        let key = matchWord.substring(0, matchWord.length)
+
+        const customParam = textParams.find((tp) => tp.key === key)
+
+        let updatedValue = matchWord.substring(1, matchWord.length - 1)
+
+        if (customParam) {
+          const customProp = props[customParam.name]
+          if (customProp) {
+            updatedValue = customProp
+          }
+        }
+
+        pieces = pieces + updatedValue
+        const lastPiece = text.slice(startIndex! + matchWord.length, text.length)
+        text = pieces + lastPiece
+        replaces()
+        return false
+      }
     }
+
     return text
   }
   replaces()
