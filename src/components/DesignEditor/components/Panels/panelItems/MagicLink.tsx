7da0b82552b52de9bfecb470ba819043a30a3e9e
Pablito Jean Pool Silva Inca <keane.pa.si.in@gmail.com>
2023-08-28 10:06:39 -0500

    add create design with ai

--- src/components/DesignEditor/components/Panels/panelItems/MagicLink.tsx
+++ src/components/DesignEditor/components/Panels/panelItems/MagicLink.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Center, Flex, Image, Text, Textarea, useDisclosure } from "@chakra-ui/react"
-import { useCallback, useState } from "react"
+import { Box, Button, Center, Flex, Image, Text, Textarea, useDisclosure, useToast } from "@chakra-ui/react"
+import { useCallback, useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import GenerateIADesign from "~/components/Modals/GenerateIADesign"
 import useDesignEditorContext from "~/hooks/useDesignEditorContext"
@@ -14,10 +14,19 @@ export default function () {
   const editor: any = useEditor()
   const { setInputActive } = useDesignEditorContext()
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const [dataText, setDataText] = useState<string>(null)
+  const [dataText, setDataText] = useState<string>("")
   const activeScene = useActiveScene()
   const [err, setErr] = useState<string>(null)
   const user = useSelector(selectUser)
+  const userPromt = localStorage.getItem("user_prompt")
+  const toast = useToast()
+
+  useEffect(() => {
+    if (userPromt || userPromt !== null || userPromt !== "") {
+      setDataText(userPromt)
+      localStorage.removeItem("user_prompt")
+    }
+  }, [userPromt])
 
   const generateDesign = useCallback(async () => {
     try {
@@ -29,7 +38,17 @@ export default function () {
       setErr(null)
       onClose()
     } catch (err) {
-      setErr("Please enter a valid text.")
+      if (err?.response?.data?.message) {
+        setErr(err?.response?.data?.message)
+      } else {
+        toast({
+          title: "NETWORK ERROR, PLEASE TRY AGAIN.",
+          status: "error",
+          position: "top",
+          duration: 3000,
+          isClosable: true
+        })
+      }
       onClose()
     }
   }, [editor, dataText, activeScene])
@@ -51,6 +70,7 @@ export default function () {
           <Text>Your Text</Text>
           <Textarea
             h="50%"
+            value={dataText}
             onFocus={() => setInputActive(true)}
             onBlur={() => setInputActive(false)}
             onChange={(e) => setDataText(e.target.value)}
